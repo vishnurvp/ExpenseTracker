60f29781e9bfbfb2092e65d5a2fb1ec732cd7f19
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-20 21:48:45 +0530

    crud_Expence_Tracker

--- script.js
+++ script.js
@@ -6,63 +6,55 @@ const addExpBtn = document.getElementById('addexpbtn');
 const dispDiv = document.getElementById('display');
 const itemList = document.getElementById('list');
 
-let data = [];
-if (localStorage.data){
-    data = JSON.parse(localStorage.data)
-}
 
-for (let i=0; i<data.length; i++) {
-    let listdata = data[i];
-    let listItem = document.createElement('li');
-    listItem.id = `${listdata.ammount} - ${listdata.description} - ${listdata.catagory} `;
-    listdata.id = listItem.id;
-    listItem.innerHTML = listItem.id;
+let crudURL = 'https://crudcrud.com/api/a45e37be7bc34c0dba17dfb85a666ff5/expenceTracker';
+axios.get(crudURL)
+.then((response)=>{
+    let expences=response.data;
+    if (expences.length===0) return; // if no data, do nothing
+    for (let expence of expences) {dispUser(expence);}
+})
+.catch(err=>console.log(err));
+
+function dispUser(expence) {
+    let li = document.createElement('li');
+    li.id = expence._id;
+    li.ammount = expence.ammount;
+    li.description = expence.description;
+    li.catagory = expence.catagory;
+    li.innerHTML = `${expence.ammount} - ${expence.description} - ${expence.catagory} `;
 
     let editbtn = document.createElement('button');
     editbtn.innerHTML = 'Edit';
     editbtn.className = 'edit';
-    listItem.appendChild(editbtn);
+    li.appendChild(editbtn);
 
     let deletebtn = document.createElement('button');
     deletebtn.innerHTML = 'Delete';
     deletebtn.className = 'delete';
-    listItem.appendChild(deletebtn);
+    li.appendChild(deletebtn);
 
-    itemList.appendChild(listItem);
+    itemList.appendChild(li);
 }
 
 addExpBtn.addEventListener('click', () => {
-    let listdata = {};
-    listdata.ammount = ammountInput.value;
-    listdata.description = descriptionInput.value;
-    listdata.catagory = catagoryInput.value;
-    // console.log(ammount, description, catagory)
-    
-    let listItem = document.createElement('li');
-    listItem.id = `${listdata.ammount} - ${listdata.description} - ${listdata.catagory} `
-    listdata.id = listItem.id
-    listItem.innerHTML = listItem.id;
-    data.push(listdata);
-    localStorage.setItem('data', JSON.stringify(data));
-
-
-    let editbtn = document.createElement('button');
-    editbtn.innerHTML = 'Edit';
-    editbtn.className = 'edit';
-    listItem.appendChild(editbtn);
-
-    let deletebtn = document.createElement('button');
-    deletebtn.innerHTML = 'Delete';
-    deletebtn.className = 'delete';
-    listItem.appendChild(deletebtn);
-
-    itemList.appendChild(listItem);
+    let expence = {};
+    if (ammountInput.value === '') {
+        return;
+    }
+    expence.ammount = ammountInput.value;
+    expence.description = descriptionInput.value;
+    expence.catagory = catagoryInput.value;
+    axios.post(crudURL,expence)
+    .then((response)=>{
+        dispUser(response.data);
+    })
+    .catch(err=>console.log(err));
 
     ammountInput.value = '';
     descriptionInput.value = '';
-
-
 });
+
 
 itemList.addEventListener('click', EditDelete);
 function EditDelete(e){
@@ -70,32 +62,18 @@ function EditDelete(e){
         let li = e.target.parentElement;
         itemList.removeChild(li);
         // console.log(data);
-        for (let i=0; i<data.length; i++) {
-            // console.log(li.id);
-            // console.log(data[i].id);
-            if (li.id === data[i].id) {
-                data.splice(i, 1);
-                break;
-            }
-        }
-        localStorage.setItem('data', JSON.stringify(data));
+        let url = `${crudURL}/${li.id}`;
+        // delete user data in crudcrud
+        axios.delete(url).catch(err=>console.log(err));
     }
     if (e.target.classList.contains('edit')) {
         let li = e.target.parentElement;
         itemList.removeChild(li);
-        let tempdata;
-        for (let i=0; i<data.length; i++) {
-            tempdata = data[i];
-            if (data[i].id === li.id) {
-                data.splice(i, 1);
-                break;
-            }
-        }
-        localStorage.setItem('data', JSON.stringify(data));
-        ammountInput.value = tempdata.ammount;
-        descriptionInput.value = tempdata.description;
-        catagoryInput.value = tempdata.catagory;
+        let url = `${crudURL}/${li.id}`;
+        axios.delete(url).catch(err=>console.log(err));
+        ammountInput.value = li.ammount;
+        descriptionInput.value = li.description;
+        catagoryInput.value = li.catagory;
     }
-
 }
 
